747eb4bb52fd6e9cd801008eb289a4e341b22664
LuboGolemiq <lubomirpalazov19@gmail.com>
2024-02-16 12:24:54 +0200

    vtora proba

---
+++ Zadachadve/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Zadachadve
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //nai golemiq element v masiva
+            Console.Write("dai broi na elementite:");
+            int n = int.Parse(Console.ReadLine());
+            int[] num = new int[n];
+            for (int i = 0; i < num.Length-1; i++)
+            {
+                Console.Write($"num[{i}]");
+                num[i] =int.Parse(Console.ReadLine());
+
+            }
+            for (int i = 0 ; i < num.Length-1;i++)
+            {
+                Console.WriteLine($"num[{i}]={num}");
+
+            }
+        }
+    }
+}
---
+++ Zadaha/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Zadaha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] grades = new double[13];
+            string[] names = new string[9] { "lubo", "Milen", "mihail", "radoslav", "svetlomir", "siana", "silviq", "stanislav", "tedo" };
+               
+            for (int i = 0; i < grades.Length; i++)
+            {
+                Console.WriteLine($"Otcenkata na {names[i]}");
+                grades[i] = double.Parse(Console.ReadLine());
+            }
+            grades[0] = 6;
+            names[0] = "lubo";
+            Console.WriteLine($"{grades[0]}{names[0]}");
+
+        }
+    }
+}
